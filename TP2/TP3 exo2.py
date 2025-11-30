5878bc1cf9a1924745b41486462d5ec643d017ba
Tapof <samuelbaur10@gmail.com>
2025-11-30 19:41:41 +0100

    maj

--- TP2/TP3 exo2.py
+++ TP2/TP3 exo2.py
@@ -1,3 +1,15 @@
-Nmbr = int(input("donnez un nombre"))
-Nmbr = Nmbr+1
-print(Nmbr)
+import time
+#somme = 0
+#Nmbr = int(input("donnez un nombre"))
+#for i in range(Nmbr, -1, -1 ):
+   # somme += i
+    #Nmbr = int(input("donnez un nombre"))
+
+
+
+n=int(input("donnez un nombre:"))
+
+while n >= 0:
+    print(n)
+    time.sleep(1)
+    n-=1
